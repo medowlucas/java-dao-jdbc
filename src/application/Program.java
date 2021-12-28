d39b4745f51305564ca69b203efd1497ebc42f17
Lucas Lopes de Souza Guilha <medowlucas@gmail.com>
2021-12-27 21:40:30 -0300

    criado a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,23 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Program {
 
-    public static void main(String[] args) {
-        Department obj = new Department(1,"Books");
-        System.out.println((obj));
+    public static void main(String[] args) throws ParseException {
+        Department department = new Department(1,"Books");
+        System.out.println((department));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date birthDate = sdf.parse("24/03/1990");
+
+
+        Seller seller = new Seller(21,"Lucas","medowlucas@gmail.com",birthDate,3000.0,department);
+        System.out.println((seller));
     }
 }
