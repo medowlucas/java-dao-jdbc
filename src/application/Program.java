c70885f2641d13414bf99ba084a5f85a53940d87
Lucas Lopes de Souza Guilha <medowlucas@gmail.com>
2022-01-03 21:52:50 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,29 +2,18 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) throws ParseException {
-        Department department = new Department(1,"Books");
-        System.out.println((department));
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Date birthDate = sdf.parse("24/03/1990");
-
-
-        Seller seller = new Seller(21,"Lucas","medowlucas@gmail.com",birthDate,3000.0,department);
-        System.out.println((seller));
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller sellerDaoByID = sellerDao.findById(3);
-        System.out.println(sellerDaoByID);
+        System.out.println("===Test 1: seller findById===");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
     }
 }
